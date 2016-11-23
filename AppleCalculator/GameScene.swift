39874dcdbef46a12114a830371ea215b805eaec3
Rasmus Gulbaek <rasmus.gulbaek@gmail.com>
2016-11-23 21:28:06 +0100

    Optimized Graphic  for performance and added result sign

--- AppleCalculator/GameScene.swift
+++ AppleCalculator/GameScene.swift
@@ -16,6 +16,9 @@ class GameScene: SKScene {
     
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
+    private var apple : SKSpriteNode?
+    private var apple1 : SKSpriteNode?
+    private var apple10 : SKSpriteNode?
     private var spinnyNode : SKShapeNode?
     
     //var apple: SKSpriteNode?
@@ -24,14 +27,16 @@ class GameScene: SKScene {
 
         self.lastUpdateTime = 0
         
-        //apple = self.childNode(withName: "Apple") as? SKSpriteNode
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloApple") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //TODO main setup
+        label = self.childNode(withName: "Result") as? SKLabelNode
+        label?.text = "2"
+        
+//        apple1 = self.childNode(withName: "Apple1") as? SKSpriteNode
+//        apple1?.physicsBody?.affectedByGravity = true
+//        
+//        apple10 = self.childNode(withName: "Apple10") as? SKSpriteNode
+//        apple10?.physicsBody?.affectedByGravity = true
         
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
@@ -73,6 +78,90 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        let touch = touches.first as UITouch?
+//        _ = touch!.location(in: self)
+//
+        let location: CGPoint! = touch!.location(in: scene!)
+//        
+//        let nodeAtPoint = self.atPoint(location)
+//        let nodesAtPoint = self.nodes(at: location)
+//        
+        
+        
+        
+//        let touch = touches.anyObject() as UITouch
+//        let touchLocation = touch.locationInNode(self)
+        let nodes = self.nodes(at: location) as [SKNode]
+        
+        for node in nodes {
+            if let nodeName = node.name {
+                if  nodeName == "Left_Apple_1" ||
+                    nodeName == "Left_Apple_2" ||
+                    nodeName == "Left_Apple_3" ||
+                    nodeName == "Left_Apple_4" ||
+                    nodeName == "Left_Apple_5" ||
+                    nodeName == "Left_Apple_6" ||
+                    nodeName == "Left_Apple_7" ||
+                    nodeName == "Left_Apple_8" ||
+                    nodeName == "Left_Apple_9" ||
+                    nodeName == "Left_Apple_10"
+                {
+                    
+                    
+                    apple = self.childNode(withName: nodeName) as? SKSpriteNode
+                    apple?.position = CGPoint(x: -230, y: 100)
+                    apple?.physicsBody?.affectedByGravity = true
+                }
+                else if  nodeName == "Right_Apple_1" ||
+                    nodeName == "Right_Apple_2" ||
+                    nodeName == "Right_Apple_3" ||
+                    nodeName == "Right_Apple_4" ||
+                    nodeName == "Right_Apple_5" ||
+                    nodeName == "Right_Apple_6" ||
+                    nodeName == "Right_Apple_7" ||
+                    nodeName == "Right_Apple_8" ||
+                    nodeName == "Right_Apple_9" ||
+                    nodeName == "Right_Apple_10"
+                {
+                    
+                    
+                    apple = self.childNode(withName: nodeName) as? SKSpriteNode
+                    apple?.position = CGPoint(x: 230, y: 100)
+                    apple?.physicsBody?.affectedByGravity = true
+                }
+                else if  nodeName == "Plus"
+                {
+                    apple = self.childNode(withName: "Left_Apple_1") as? SKSpriteNode
+                    apple?.position = CGPoint(x: -213, y: 381)
+                    apple?.physicsBody?.affectedByGravity = false
+                    
+                    
+                    apple = self.childNode(withName: "Left_Apple_2") as? SKSpriteNode
+                    apple?.position = CGPoint(x: -106, y: 475)
+                    apple?.physicsBody?.affectedByGravity = false
+                    
+                    apple = self.childNode(withName: "Left_Apple_3") as? SKSpriteNode
+                    apple?.position = CGPoint(x: -128, y: 495)
+                    apple?.physicsBody?.affectedByGravity = false
+
+
+                }
+                else if  nodeName == "Equal"
+                {
+                    apple = self.childNode(withName: "TopDoor") as? SKSpriteNode
+                    apple?.removeFromParent()
+                    
+                    
+                }
+            }
+        }
+        
+        
+        
+        //print("nodeAtPoint.name: " + nodeAtPoint.name! )
+        //print(nodesAtPoint.count )
+        
         if let label = self.label {
             label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
         }
